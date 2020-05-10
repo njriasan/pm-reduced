5dae0b480ad0f4d21760a7e34db013af85375385
njriasan <njriasanovsky@berkeley.edu>
2020-05-10 04:54:06 -0700

    trying to fix setup

--- setup/setup.sh
+++ setup/setup.sh
@@ -5,11 +5,11 @@
 # - on OSX: /Users/<user>/miniconda3/bin/conda
 # - on Windows: C:/Users/<user>/Miniconda3/Scripts/conda
 
+. setup/check_for_conda.sh
 
 echo "Setting up blank environment"
 conda create --name emission-pm python=3.6
+conda activate emission-pm
 
 echo "Updating using conda now"
 conda env update --name emission-pm --file setup/environment36.yml
-echo "Initializing using conda now"
-conda init bash
